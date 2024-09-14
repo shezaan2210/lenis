81be1456eadc55abc67968a93fe1d83d1497c036
shezaan <shezaanansari2210@gmail.com>
2024-09-15 00:02:49 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+const lenis = new Lenis({
+    duration: 4
+});
+
+lenis.on("scroll", (e) => {
+  
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+document.querySelectorAll(".elem").forEach((elem) => {
+  let image = elem.querySelector("img");
+   let tl =  gsap.timeline()
+   let xTransform = gsap.utils.random(-100, 100)
+
+        tl
+        .set(image, {
+            transformOrigin: `${xTransform < 0 ? 0 : '100%'}`,
+        },'start')
+        .to(image, {
+            scale: 0,
+            ease: 'none',
+            scrollTrigger: {
+                trigger: image,
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+                // markers: true
+            }
+        }, 'start')
+        .to(elem, {
+            xPercent: xTransform,
+            ease: 'none',
+            scrollTrigger: {
+                trigger: image,
+                start: 'top bottom',
+                end: 'bottom top',
+                scrub: true,
+                // markers: true
+            }
+        }, 'start')
+});
